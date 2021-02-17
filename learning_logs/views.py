6ffe3f84049ec72abea1206bbb32d96f9e9fb500
Abhishek Singh <thakur89232363@gmail.com>
2021-02-17 21:21:33 +0530

    change in viewa.py

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from .forms import TopicForm,EntryForm
 from .models import Topic,Entry
 from django.contrib.auth.decorators import login_required 
@@ -22,7 +22,7 @@ def topics(request):
 
 @login_required
 def topic(request,topic_id):
-    topic=Topic.objects.get(id=topic_id)
+    topic=get_object_or_404(Topic,id=topic_id)
     if topic.owner != request.user:
         raise Http404
     entries=topic.entry_set.order_by('-date_added')
